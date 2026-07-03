2ef74e4494e5dffd26ff4f80b622d8db85d866cd
Davi-Pedrosa <daviespinosa037@gmail.com>
2026-07-03 08:32:53 -0300

    LGA - API connection and response successful

--- main.py
+++ main.py
@@ -5,15 +5,33 @@ from service.google_places import buscar_empresas
 
 
 def main():
-    resultado = buscar_empresas("São Paulo")
-
-    for empresa in resultado.get("places", []):
-        print("-" * 40)
-        print(f"Nome: {empresa.get('name')}")
-        print(f"Endereço: {empresa.get('formatted_address')}")
-        print(f"Nível de Preço{empresa.get('priceLevel')}")
-        print(f"Avaliação: {empresa.get('rating')}")
-        print("-" * 40)
+    query_busca = "Barbearias Limeira"
+    print(f"Buscando leads para a query: {query_busca}")
+    resultado = buscar_empresas(query_busca)
+    
+    lista_leads = resultado.get("results") or resultado.get("places") or []
+    
+    if not lista_leads:
+        print("Nenhum lead encontrado.")
+        return
+    
+    print(f"Leads encontrados: {len(lista_leads)}. Mapeando os dados:")
+    
+    for empresa in lista_leads:
+        print(f'-'*50)
+        nome = empresa.get('displayName',{}).get('text', 'Sem nome')
+        print(f"Nome: {nome}")
+        print(f"Endereço:{empresa.get('formattedAddress', 'Sem endereço')}")
+        print(f"Telefone: {empresa.get('nationalPhoneNumber', 'Sem telefone')}")
+        site = empresa.get('websiteUri')
+        if site:
+            print(f"Website: {site}")
+        else:
+            print("Website: 🚨 NÃO POSSUI SITE 🚨")
+        print(f"Avaliação: {empresa.get('rating', 'Sem avaliação')}")
+        print(f"Nível de Preço: {empresa.get('priceLevel', 'Sem nível de preço')}")
+        print(f'-'*50)
+        input("Pressione Enter para continuar...")
         
 if __name__ == "__main__":
     main()
--- service/google_places.py
+++ service/google_places.py
@@ -8,15 +8,19 @@ URL = "https://places.googleapis.com/v1/places:searchText"
 def buscar_empresas(cidade: str):
     headers = {
         "Content-Type": "application/json",
-        "X-Google-Places-Api": settings.GOOGLE_API_KEY,
+        "X-Goog-Api-Key": settings.GOOGLE_API_KEY,
         # Campos que retornam da api
-        "X-Goog-FieldMask":"places.displayName,places.formattedAddress,places.priceLevel,places.websiteUri,places.phoneNumber,places.rating,places.reviews,places.openingHours"
+        "X-Goog-FieldMask":"places.displayName,places.formattedAddress,places.priceLevel,places.websiteUri,places.nationalPhoneNumber,places.rating"
     }
+    
     body = {
         "textQuery": f"empresas em {cidade}",
     }
     
     response = requests.post(URL, headers=headers, json=body)
     
+    if response.status_code != 200:
+        print(f"Erro na requisição: {response.status_code} - {response.text}")
+        return {}
     # response.raise_for_status()  # Levanta uma exceção se a resposta não for bem-sucedida
     return response.json()
