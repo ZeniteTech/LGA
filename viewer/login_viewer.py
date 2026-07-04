b5f57c212fd7a99f6c9ba1af6428b1880bc126d3
Davi-Pedrosa <daviespinosa037@gmail.com>
2026-07-04 20:38:11 -0300

    added: login screen and system control

---
+++ viewer/login_viewer.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv(override = True)
+
+def login():
+    senha=os.getenv("LOGIN_LGA")
+    if "logado" not in st.session_state:
+        st.session_state.logado = False
+
+    if st.session_state.logado:
+        return True
+
+    st.title("ZenithTech - LGA")
+    st.subheader("Login de Acesso")
+    input_senha = st.text_input("Informe a senha de acesso", type="password", key="senha_login")
+    botao_envio = st.button("Entrar")
+
+    if botao_envio:
+        if input_senha == senha:
+            st.session_state.logado = True
+            st.success("Senha correta! Aguarde...")
+            time.sleep(1)
+            st.rerun()
+        else:
+            st.error("Senha incorreta.")
+
+    return False
+    
