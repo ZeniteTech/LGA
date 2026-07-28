96a3e1b2f5f7f91fcb7d00a50fe368cdeff43365
Kaua <kauaoliveira18062007@gmail.com>
2026-07-28 08:30:20 -0300

    feat: limite de busca de leads aumentado para 60

--- service/google_places.py
+++ service/google_places.py
@@ -1,26 +1,44 @@
+import time
 import requests
-
-# from config.settings import GOOGLE_API_KEY
 from config.settings import settings
 
 URL = "https://places.googleapis.com/v1/places:searchText"
 
-def buscar_empresas(cidade: str):
+def buscar_empresas(query: str, limite=60):
     headers = {
         "Content-Type": "application/json",
         "X-Goog-Api-Key": settings.GOOGLE_API_KEY,
-        # Campos que retornam da api
-        "X-Goog-FieldMask":"places.displayName,places.formattedAddress,places.priceLevel,places.websiteUri,places.nationalPhoneNumber,places.rating"
+        # Campos que retornam da API
+        "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.priceLevel,places.websiteUri,places.nationalPhoneNumber,places.rating,nextPageToken"
     }
+
+    places = []
+    page_token = None
+
+    while len(places) < limite:
+        payload = {
+            "textQuery": query,
+            "pageSize": min(20, limite - len(places))
+        }
+
+        if page_token:
+            payload["pageToken"] = page_token
     
-    body = {
-        "textQuery": f"empresas em {cidade}",
-    }
-    
-    response = requests.post(URL, headers=headers, json=body)
-    
-    if response.status_code != 200:
-        print(f"Erro na requisição: {response.status_code} - {response.text}")
-        return {}
-    # response.raise_for_status()  # Levanta uma exceção se a resposta não for bem-sucedida
-    return response.json()
+        response = requests.post(URL, headers=headers, json=payload).json()
+        
+        # Pega a lista da página atual
+        novos_places = response.get("places", [])
+        
+        # Adiciona os novos lugares na lista acumuladora 'places'
+        places.extend(novos_places)
+
+        # Atualiza o token para a próxima requisição
+        page_token = response.get("nextPageToken")
+
+        # Se não houver mais token ou se a busca não trouxe novos resultados, encerra
+        if not page_token or not novos_places or len(places) >= limite:
+            break
+
+        time.sleep(2)
+
+    return {"places": places[:limite]}
