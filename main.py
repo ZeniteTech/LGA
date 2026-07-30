da64ab5a5eca55cc51894067b81b3ef9ea2e7314
Davi-Pedrosa <163602669+Davi-Pedrosa@users.noreply.github.com>
2026-07-30 14:22:10 -0300

    Update CORS allowed origin to localhost:8081

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:8081"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
